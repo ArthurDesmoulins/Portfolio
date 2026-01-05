512afa45d6475a71837c4a6158040e1d186173ba
ArthurDesmoulins <arthur.desmoulins@etu.unilim.fr>
2026-01-05 20:02:16 +0100

    fix: correct typo from 'projets.html' to 'projects.html' in Vite build input

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        projets: 'projets.html'
+        projets: 'projects.html'
       }
     }
   }
